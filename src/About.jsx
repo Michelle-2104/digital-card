3670cc530a43e079adf597f1036105c2c3325dd8
Michelle2104 <chidowemichelle@gmail.com>
2022-12-19 21:05:32 +0200

    .

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className='about-div'>
         <h1>About me</h1>
     <p>
-    I am a professional lady aged 32 who is well versed in Front-end development. 
+    I am a professional lady who is well versed in Front-end development. 
     I have five years experience in React, Javascript, CSS and HTML. 
     </p>
     </div>
