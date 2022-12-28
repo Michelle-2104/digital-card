a18e4a5fbfac2d01d2cf1a21774cb7ae6e6194b8
Michelle2104 <chidowemichelle@gmail.com>
2022-12-28 19:46:49 +0200

    .

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function Footer() {
   return (
-   
-   <div className='footer-icons'>
-      <a href='#'>
-         <i class="fa-brands fa-facebook"></i>
+     <div className='footer-icons'>
+        <a href='https://github.com/Michelle-2104'>
+           <i className="fa-brands fa-github"></i>
+        </a>
+
+     <a href='https://discord.com/channels/@me'>
+        <i class="fa-brands fa-discord"></i>      
       </a>
 
-      <a href='#'>
-         <i class="fa-brands fa-twitter"></i> 
+      <a href='https://twitter.com/MichelleChidowe'>
+         <i className="fa-brands fa-twitter"></i> 
       </a>
 
-      <a href='#'>
-          <i class="fa-brands fa-instagram"></i>
-      </a>
-      <a href='#'>
-         <i class="fa-brands fa-github"></i>
+      <a href='https://www.instagram.com/michelletinevimbo/'>
+          <i className="fa-brands fa-instagram"></i>
       </a>
     </div>
   )
